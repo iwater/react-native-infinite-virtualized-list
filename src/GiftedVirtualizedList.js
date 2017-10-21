fcc261d13104780892db4c273197e0db6a6b1911
iwater <iwater@gmail.com>
2017-10-22 00:01:26 +0800

    fix for react-native >= 0.49

--- src/GiftedVirtualizedList.js
+++ src/GiftedVirtualizedList.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import InfiniteVirtualizedList from './InfiniteVirtualizedList'
 
 export default class GiftedVirtualizedList extends React.Component {
--- src/InfiniteVirtualizedList.js
+++ src/InfiniteVirtualizedList.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { VirtualizedList } from 'react-native'
 import { InfiniteLoader } from 'react-virtualized/dist/commonjs/InfiniteLoader'
 
